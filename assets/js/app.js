c358afc4d085f7c93ba0495f59914471f2d9c6f3
vanthom04 <vanthom2210@gmail.com>
2023-06-05 17:20:43 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,14 @@
 // =================== Menu ===================
 const navMenu = document.querySelector(".navbar");
+const menu = document.querySelector(".bi-list");
 
 function hideMenu() {
   navMenu.style.right = "-500px";
+  menu.style.display = "block";
 }
 function showMenu() {
   navMenu.style.right = "0";
+  menu.style.display = "none";
 }
 
 // =================== Login link ===================
